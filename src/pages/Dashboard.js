69fa0f2a7cec095ba3df37f4a038bd2f52971426
Rosslyn Taghoy <rosslyn.taghoy@gmail.com>
2018-09-27 21:55:18 +0800

    profile

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import AuthorizedLayout from '../layouts/AuthorizedLayout';
+// import AuthorizedLayout from '../layouts/AuthorizedLayout';
 import { inject, observer } from 'mobx-react';
 
 @inject('store') @observer
@@ -9,7 +9,7 @@ class Dashboard extends Component {
             // <AuthorizedLayout>
                 <div>
                     <p>Hello, {this.props.store.userStore.name}</p>
-                    {/* <p>Your email is: { this.props.store.userStore.email }</p> */}
+                    <p>Your email is: { this.props.store.userStore.email }</p>
                     <img alt='Profile' src={this.props.store.userStore.profilePicture} />
                 </div>
             // </AuthorizedLayout>
