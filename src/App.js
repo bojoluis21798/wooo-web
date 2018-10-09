ac673245f0f98ccb824d049e32cc4bd6e287c476
jacklaurencegaray <lawrencetecho@gmail.com>
2018-10-10 01:35:03 +0800

    CI Stress Test #1

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Dashboard from "./pages/Dashboard";
 import EditProfile from "./pages/editProfile";
 import Loading from "./pages/Loading";
 import Matching from "./pages/matchingUI";
+import Testing from './pages/Testing'
 
 class App extends Component {
   state = { loading: true };
@@ -29,6 +30,7 @@ class App extends Component {
             <Route path="/edit-profile" component={EditProfile} />
             <Route path="/loading" component={Loading} />
             <Route path="/matching" component={Matching} />
+            <Route path='/testing' component={Testing} />
             <Route path="*" component={Login} />
           </Switch>
         </Router>
---
+++ src/pages/Testing.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Testing extends Component {
+  render() {
+    return (
+      <div>
+        Deployment Stress Test #1
+      </div>
+    )
+  }
+}
