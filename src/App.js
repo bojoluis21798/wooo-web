a2eb24310e63eb9092ecd8eaca2197b66e92cc32
bojoluis21798 <trinity21798@gmail.com>
2018-10-09 23:46:50 +0800

    Route to matching

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import EditProfile from "./pages/editProfile";
 import Loading from "./pages/Loading";
-
+import Matching from "./pages/matchingUI";
 
 class App extends Component {
   state = { loading: true };
@@ -22,12 +22,13 @@ class App extends Component {
       <Loading message="Preparing the app.." />
     ) : (
       <Provider store={RootStore}>
-        <Router>  
+        <Router>
           <Switch>
             <Route exact path="/login" component={Login} />
             <Route path="/dashboard" component={Dashboard} />
             <Route path="/edit-profile" component={EditProfile} />
             <Route path="/loading" component={Loading} />
+            <Route path="/matching" component={Matching} />
             <Route path="*" component={Login} />
           </Switch>
         </Router>
