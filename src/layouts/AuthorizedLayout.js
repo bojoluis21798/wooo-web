ad3d62d680cabca101d62408a43184204ab04e55
jacklaurencegaray <lawrencetecho@gmail.com>
2018-10-09 16:18:25 +0800

    Fixed authorized layout; added headers to it

--- src/layouts/AuthorizedLayout.js
+++ src/layouts/AuthorizedLayout.js
@@ -2,15 +2,36 @@
 import React, { Component } from 'react'
 import { inject, observer } from 'mobx-react'
 import { Redirect } from 'react-router-dom'
+import styled from 'styled-components'
+import Header from '../components/Header'
 
 @inject('store') @observer
 export default class AuthorizedLayout extends Component {
   
-    isAuthorized = () => this.props.store.userStore.email
+    isAuthorized = () => this.props.store.userStore.auth_token
 
     render() {
-        return this.isAuthorized()? this.props.children: (
+        return !this.isAuthorized()? 
             <Redirect to='/login'></Redirect>
-        )
+            : <AuthorizedContent>
+                <Header />
+                <ContentContainer>
+                    { this.props.children }
+                </ContentContainer>
+            </AuthorizedContent>
     }
 }
+
+const AuthorizedContent = styled.div`
+    background-color: #111111;
+    min-height: 100vh;
+    color: #ffffff;
+    max-width: 600px;
+    margin-left: auto;
+    margin-right: auto;
+`
+
+const ContentContainer = styled.div`
+    padding-left: 10px;
+    padding-right: 10px;
+`
