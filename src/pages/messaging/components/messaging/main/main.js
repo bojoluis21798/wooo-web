aad90220f0e6e2558d59eda1207bdca6957fb158
CakeShip <cake.kgc@gmail.com>
2018-10-17 19:51:50 +0800

    Fix prop

--- src/pages/messaging/components/messaging/main/main.js
+++ src/pages/messaging/components/messaging/main/main.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 import './main.css';
 import {MessageBody} from '../message/main';
 
-
 export class Messaging extends Component {
+  state = {
+    props: this.props.location.state,
+  };
+
   componentDidMount(){
-    console.log(this.props.name)
+    console.log("MAIN");
+    console.log(this.state.props.name);
   }
   render() {
     return (
       <div>
-        <MessageBody props={this.props}/>
+        <MessageBody/>
       </div>
     );
   }
