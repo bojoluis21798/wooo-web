2c8e5347c926072dfa27ab9699325ad647307604
CakeShip <cake.kgc@gmail.com>
2018-10-17 21:31:03 +0800

    DB nlng

--- src/pages/messaging/components/messaging/messages/main.js
+++ src/pages/messaging/components/messaging/messages/main.js
@@ -6,21 +6,37 @@ import '../../../global/global.css';
 
 @inject('store') @observer
 export default class Messages extends Component {
-  
+  help(){
+    console.log()
+  }
   render() {
     const img = require('../../../imgs/'+this.props.img);
     return (
       <div>
-        <div className="content">
-            <div className="div-3">
-              <img src={img} alt={this.props.profile_id}/>
+        {this.props.profile_id !== this.props.store.userStore.profile_id && (
+          <div className="content">
+            <div className="div-2">
+            <img src={img} alt={this.props.profile_id}/>
             </div>
-            <div  className="div-7 details1">
-              <div className="message">
+            <div  className="div-8 details2">
+              <div>
                 <p>{this.props.message.content}</p>
               </div>
             </div>
           </div>
+        )}
+        {this.props.profile_id === this.props.store.userStore.profile_id && (
+          <div className="content">
+            <div  className="div-8 details1">
+              <div>
+                <p>{this.props.message.content}</p>
+              </div>
+            </div>
+            <div className="div-2">
+              <img src={img} alt={this.props.profile_id}/>
+            </div>
+          </div>
+        )}
       </div>
     );
   }
