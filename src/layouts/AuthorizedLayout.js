041042b3f8e7b378b3d34d2a305313e5a4995024
jacklaurencegaray <lawrencetecho@gmail.com>
2018-10-24 22:54:46 +0800

    Fixed authentication

--- src/layouts/AuthorizedLayout.js
+++ src/layouts/AuthorizedLayout.js
@@ -4,29 +4,42 @@ import { inject, observer } from 'mobx-react'
 import { Redirect } from 'react-router-dom'
 import styled from 'styled-components'
 import Header from '../components/Header'
+import LoaderWrapper from './LoaderWrapper'
+import { withRouter } from 'react-router-dom'
 
 @inject('store') @observer
-export default class AuthorizedLayout extends Component {
+class AuthorizedLayout extends Component {
   
     isAuthorized = () => this.props.store.userStore.token
+
+    componentDidMount() {
+        if(!this.isAuthorized()) 
+            if(this.props.redirectTo) 
+                this.props.store.userStore.setRedirectTo(this.props.redirectTo)
+            else 
+                this.props.store.userStore.setRedirectTo(this.props.location.pathname)
+    }
 
     render() {
         return !this.isAuthorized()? 
             <Redirect to='/login'></Redirect>
-            : <AuthorizedContent>
-                { !this.props.noheaders? (<Header />): '' }
-                <ContentContainer>
-                    { this.props.children }
-                </ContentContainer>
-            </AuthorizedContent>
+            : <LoaderWrapper>
+                <AuthorizedContent>
+                    { !this.props.noheaders? (<Header />): '<div></div>' }
+                    <ContentContainer>
+                        { this.props.children }
+                    </ContentContainer>
+                </AuthorizedContent>
+            </LoaderWrapper>
     }
 }
+
+export default withRouter(AuthorizedLayout)
 
 const AuthorizedContent = styled.div`
     background-color: #111111;
     min-height: 100vh;
     color: #ffffff;
-    max-width: 600px;
     margin-left: auto;
     margin-right: auto;
 `
