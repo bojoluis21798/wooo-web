5a6a95429b83578cc08a25b130a1689ae1a4b5f1
bojoluis21798 <trinity21798@gmail.com>
2018-10-25 14:48:17 +0800

    Made method async

--- src/stores/UserStore.js
+++ src/stores/UserStore.js
@@ -90,7 +90,7 @@ class UserStore {
     }
 
     @action
-    getLocation(){
+    await getLocation(){
         try{
             navigator.geolocation.getCurrentPosition((position) => {
               this.setLocation( {
