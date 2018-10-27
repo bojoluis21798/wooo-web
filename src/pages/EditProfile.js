bdcdde5db01b7fdf1a0b31ec273dc0f95d85e521
Kobeboy15 <kobemichael15@gmail.com>
2018-10-27 16:47:12 +0800

    Adding back the gender-toggle and the upload photos

--- src/pages/EditProfile.js
+++ src/pages/EditProfile.js
@@ -3,6 +3,7 @@ import styled, {css} from "styled-components"
 import { inject, observer } from 'mobx-react'
 import { ToastContainer } from "react-toastify"
 import Slider from "rc-slider"
+import Toggle from 'react-switch'
 import "rc-slider/assets/index.css"
 import axios from 'axios'
 import AuthorizedLayout from '../layouts/AuthorizedLayout'
@@ -10,31 +11,75 @@ import AuthorizedLayout from '../layouts/AuthorizedLayout'
 @inject('store') 
 @observer
 class EditProfile extends Component {
-  onFormSubmit = (e) =>{
-    e.preventDefault() 
-    this.fileUpload(this.state.file).then((response)=>{
-    })
-  }
+  // onFormSubmit = (e) =>{
+  //   e.preventDefault() 
+  //   this.fileUpload(this.state.file).then((response)=>{
+  //   })
+  // }
 
-  onChange = (e) => {
-    this.setState({file:e.target.files[0]})
+  // onChange = (e) => {
+  //   this.setState({file:e.target.files[0]})
+  // }
+  componentDidMount(){
+    console.log("This is Profile")
   }
 
   handleSubmit = (e = null) => {
-    if (e !== null){
-      e.preventDefault();
-    }
+    const token = this.props.store.userStore.token;
     const config = {
         headers: {
-            Authorization: 'Token ' + this.props.store.userStore.token
+            'Authorization': 'Token ' + token,
         }
     }
-
+    console.log("Axios --PUT")
     axios.put(`${process.env.REACT_APP_API_BASEURL}/profiles/${this.props.store.userStore.profile_id}/`, {
       bio:this.props.store.userStore.biography,
       sexual_preference:this.props.store.userStore.preference,
-      search_radius:this.props.store.userStore.radius
-    }, config)
+      gay:this.props.store.userStore.gay,
+      search_radius:this.props.store.userStore.radius,
+    },config)
+    .then(response => {
+      console.log("PUT was Successful!");
+
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+
+  handleSubmitImage = () => {
+    const token = this.props.store.userStore.token;
+    const config = {
+        headers: {
+            'Authorization': 'Token ' + token,
+            'content-type': 'multipart/form-data'
+            
+        }
+    }
+    const fd = new FormData();
+    fd.append('suporting_pic_1',this.props.store.userStore.photos[0])
+    const url = `${process.env.REACT_APP_API_BASEURL}/profiles/${this.props.store.userStore.profile_id}/`;
+
+    axios.put(url,fd,config)
+    .then(response => {
+      console.log("-- PUT PHOTO --");
+      console.log(response);
+    })
+    .catch(error => {
+      console.log("-- ERROR PHOTO --");
+      console.log(error);
+    })
+
+  }
+
+  handleImageOne = (event) => {
+    console.log("----BEGIN HANLDER----")
+    const store = this.props.store.userStore;
+    
+    store.setPicOne(event.target.files[0])
+    this.handleSubmitImage()
+
+    console.log("----END HANDLER----")
   }
 
   handleMale = (e) => {
@@ -49,6 +94,18 @@ class EditProfile extends Component {
 
   handleOthers = (e) => {
     this.props.store.userStore.setPreference(2)
+    this.handleSubmit(e)
+  }
+
+  handleGay = (e) => {
+    const store = this.props.store.userStore;
+
+    if(store.gay === false) {
+      store.setGay(true)
+    } else {
+      store.setGay(false)
+    }
+    
     this.handleSubmit(e)
   }
 
@@ -80,6 +137,20 @@ class EditProfile extends Component {
               <Tagline>Photos</Tagline>
               <ProfileImage>
                   <ProfileImageMain alt='Profile' src={this.props.store.userStore.profilePicture} />
+                  <Image 
+                    id="img1" 
+                    style={ { backgroundImage: "url('this.props.store.userStore.photos[0]')" } } 
+                    onClick={(e) =>{this.refs.fileUploader.click();}} 
+                  >
+                    <input 
+                      id="imageOne"
+                      type="file" 
+                      ref="fileUploader" 
+                      value={this.props.store.userStore.photos[0]} 
+                      style={{display:"none"}} 
+                      onChange={this.handleImageOne}
+                    />
+                  </Image>
               </ProfileImage>
               <Tagline>Bio</Tagline>
               <BioText
@@ -110,6 +181,24 @@ class EditProfile extends Component {
                   onClick={this.handleOthers}
                   active = {this.props.store.userStore.preference === 2}
               >Others</PrefButtonOthers>
+              <label htmlFor="normal-switch">
+                  <span style={{
+                                marginRight: "10px"
+                  }}>Gay</span>
+                  <Toggle className="gayToggle"
+                      id="normal-switch"
+                      aria-label="gay"
+                      onColor="#f51a63"
+                      offColor="#191919"
+                      uncheckedIcon={false}
+                      checkedIcon={true}
+                      bottom= "-10px"
+                      checked={this.props.store.userStore.gay === true}
+                      onChange={this.handleGay}
+
+                  />
+                <label style={{paddingLeft:"10px"}}>Gay</label>
+              </label>
               <Tagline>Radius</Tagline>
               <RadiusNum>{this.props.store.userStore.radius} Km</RadiusNum>
               <br/>
@@ -191,6 +280,42 @@ const ProfileImageMain = styled.img`
   margin: auto;
 `;
 
+const Image = styled.div`
+
+  width: 77px;
+  height: 77px;
+  background-color: #191919
+  border-color: #191919
+  border-radius: 5px;
+  border: none;
+  margin: auto;
+  margin-left: 2.7%;
+  float: right;
+
+  &:hover {
+    cursor: pointer;
+    background-position: 300px;
+    background-color:  #191919;
+    border: 1px solid #f51a63;
+  }
+  &:focus {
+    outline: none !important;
+    border: 1px solid #f51a63 !important;
+  }
+
+  &#img3 {
+    margin-top: 2.5%;
+  }
+  &#img4 {
+    margin-top: 2.5%;
+    margin-left: 2.5%;
+  }
+  &#img2 {
+    margin-left: 2.5%;
+  }
+`;
+
+
 const BioText = styled.textarea`
   height: 90px;
   max-height: 150px;
@@ -205,11 +330,10 @@ const BioText = styled.textarea`
   border: none;
   justify-items: center;
   overflow: hidden;
-  resize: hidden;
+  resize: none;
 
   &:focus {
     outline: none !important;
-    border: 1px solid #f51a63 !important;
   }
 `;
 const PrefButtonMale = styled.button`
